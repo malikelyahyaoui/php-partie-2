d68ffee8cde01a5924bbb54fff360a851cc3f32d
malikelyahyaoui <elyahyaoui.malik67@gmail.com>
2024-10-03 10:41:47 +0200

    Update exo6prt.php

--- exo6prt.php
+++ exo6prt.php
@@ -1,3 +1,12 @@
+<h1>Exercice 6</h1>
+
+<p>Créer une fonction personnalisée permettant de remplir une liste déroulante à partir d'un tableau 
+de valeurs.</p>
+
+<h2>Resultat</h2>
+
+
+
 <?php
 function alimenterListeDeroulante($elements) {
     echo '<select>';
