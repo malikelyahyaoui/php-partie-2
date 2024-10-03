29e2431579431f6b7d8457c35b0bb9afe8dfd119
malikelyahyaoui <elyahyaoui.malik67@gmail.com>
2024-10-03 10:41:05 +0200

    Update exo5prt.php

--- exo5prt.php
+++ exo5prt.php
@@ -1,3 +1,11 @@
+<h1>Exercice 5</h1>
+
+<p>Créer une fonction personnalisée permettant de créer un formulaire de champs de texte en 
+précisant le nom des champs associés.</p>
+
+<h2>Resultat</h2>
+
+
 <?php
 function afficherInput($nomsInput) {
     // Commence le formulaire
